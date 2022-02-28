5764f07733510cb18028dc01fb1187b647e55490
vitinhoforall <vitinhopower12@gmail.com>
2022-02-28 14:54:06 -0300

    teste local

--- Poema/poema.js
+++ Poema/poema.js
@@ -1,8 +1,13 @@
+//Classe de modelo para os poemas
 class Poema {
     constructor(){  //toda vez que a class é chamada o constructor cria um novo objeto
-        this.name = "";
+        this.title = "";
         this.image = ""; //link src
         this.date = "";
         this.text = "";
+    }
+    saveAll(poemas = []) {
+        var jsonPoema = JSON.stringify(poemas);
+        localStorage.setItem("poemas", jsonPoema); 
     }
 }
