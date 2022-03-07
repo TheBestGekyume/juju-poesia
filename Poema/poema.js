89c208bdb7719bac8783e7dbb2d84924fb843580
vitinhoforall <vitinhopower12@gmail.com>
2022-03-07 19:31:57 -0300

    try

--- Poema/poema.js
+++ Poema/poema.js
@@ -6,8 +6,36 @@ class Poema {
         this.date = "";
         this.text = "";
     }
-    saveAll(poemas = []) {
-        var jsonPoema = JSON.stringify(poemas);
-        localStorage.setItem("poemas", jsonPoema); 
+
+    //CRUD (Create, Read, Update, Delete)
+
+    //Adicionar poema ao armazenamento
+    add(poema) {
+        
+        // var basePoema = []; //Vetor(Array) que recebe os dados do localstorage
+        // basePoema = JSON.parse(localStorage.getItem("poemas"));
+        // poema = JSON.stringify(poema); //converte os obejtos para JSON
+        // basePoema.push(poema)
+        try {
+            var basePoema = [];  //Vetor(Array) que recebe os dados do localstorage
+            basePoema = JSON.parse(localStorage.getItem('poemas'));//JSON.parse() -> pega um json e converte em objeto
+            basePoema.push(poema); //Adicionando o poema na lista de poemas;
+
+            var poemasJson = JSON.stringify(basePoema); //Criando JSON dos objetos na basePoema
+                localStorage.setItem('poemas', poemasJson);
+                return true;
+        } catch (ex) {
+            console.error(ex);
+            return false;
+        }
     }
+
+    //listar poemas
+    getAll() {
+        return JSON.parse(localStorage.getItem('poemas'));
+    }
+
+    //atualizar poemas
+
+    //remover poemas
 }
--- Poema/poemaController.js
+++ Poema/poemaController.js
@@ -1,13 +1,38 @@
 //Controle conteudo do HTML para o JavaScript
 const poemaController = {
-    poemas:[], //um array que funciona como uma lista de poemas que armazena-os localmente
+    poemas:[], //um array que funciona como uma lista de poemas que armazena-os
     adicionar:function() {
         var poema = new Poema(); // new Poema executa o constructor do poema.js
         poema.title = document.getElementById("title").value;
         poema.image = document.getElementById("image").value;
         poema.date = document.getElementById("date").value;
         poema.text = document.getElementById("text").value;
-        this.poemas.push(poema); //manda o poema que foi cadastrado para poemas
-        poema.saveAll(this.poemas);
+        // this.poemas.push(poema); //manda o poema que foi cadastrado para poemas
+        if (poema.add(poema)){
+            alert("Poema registrado!");
+        }
+        this.getAllPoema();
+    },
+    getAllPoema: function () {
+        var poema = new Poema;
+        this.poemas = poema.getAll();
+        var tabela = "<table class='table'> <tr> <th>Título</th> <th>imagem</th> <th>Data</th> <th>Texto</th> </tr>";
+
+        for (var index = 0; index < this.poemas.length; index++) {
+            tabela += "<tr> <td>" + this.poemas[index].nome + "</td> <td>" + this.poemas[index].descricao + "</td> <td>" + 
+             this.poemas[index].quant + "</td> <td> R$" + this.poema[index].valor.toFixed(2) + "</td> </tr>";
+        }
+
+        tabela += "</table>";
+        document.getElementById("listaPoemas").innerHTML = tabela
+    },
     }
-};
+
+  
+
+
+    //listar poemas
+
+    //atualizr poemas
+
+    //remover poemas
