63edcb7c6b9b406758e936a5a32935bb211fbcfa
vitinhoforall <vitinhopower12@gmail.com>
2022-03-09 18:47:38 -0300

    yau

--- Poema/poema.js
+++ Poema/poema.js
@@ -9,33 +9,57 @@ class Poema {
 
     //CRUD (Create, Read, Update, Delete)
 
-    //Adicionar poema ao armazenamento
+    //ADICIONAR POEMAS AO ARMAZENAMNETO
     add(poema) {
-        
-        // var basePoema = []; //Vetor(Array) que recebe os dados do localstorage
-        // basePoema = JSON.parse(localStorage.getItem("poemas"));
-        // poema = JSON.stringify(poema); //converte os obejtos para JSON
-        // basePoema.push(poema)
         try {
-            var basePoema = [];  //Vetor(Array) que recebe os dados do localstorage
-            basePoema = JSON.parse(localStorage.getItem('poemas'));//JSON.parse() -> pega um json e converte em objeto
+            this.validData();
+         
+            // var basePoema = [];  //Vetor(Array) que recebe os dados do localstorage
+            var basePoema = JSON.parse(localStorage.getItem('poemas'));//JSON.parse() -> pega um json e converte em objeto
+            if(basePoema == null){
+                basePoema = []
+            };
             basePoema.push(poema); //Adicionando o poema na lista de poemas;
 
             var poemasJson = JSON.stringify(basePoema); //Criando JSON dos objetos na basePoema
                 localStorage.setItem('poemas', poemasJson);
                 return true;
-        } catch (ex) {
+        }  catch (ex) {
             console.error(ex);
-            return false;
+            throw ex;
         }
     }
 
-    //listar poemas
+    
+
+    //LISTAR POEMAS
     getAll() {
-        return JSON.parse(localStorage.getItem('poemas'));
+        var basePoema = JSON.parse(localStorage.getItem('poemas'));//JSON.parse() -> pega um json e converte em objeto
+            if(basePoema == null){
+                basePoema = [] //Criando Vetor para receber os dados do localstorage 
+            }
+        return basePoema;
     }
 
     //atualizar poemas
 
     //remover poemas
-}
+
+    //validar dados
+
+    validData(){
+        var erros = ""
+        if(!this.title || this.title == "" ){
+            erros+="Poema sem título. \n"
+        }if(!this.image || this.image == "" ){
+            erros+="Poema sem imagem. \n"
+        }if(!this.date || this.date == "" ){
+            erros+="Poema sem data. \n"
+        }if(!this.text || this.text == "" ){
+            erros+="Poema sem texto. \n"
+        }
+        if(erros!= ""){
+            throw erros;
+        }
+    }
+} 
--- Poema/poemaController.js
+++ Poema/poemaController.js
@@ -2,36 +2,41 @@
 const poemaController = {
     poemas:[], //um array que funciona como uma lista de poemas que armazena-os
     adicionar:function() {
-        var poema = new Poema(); // new Poema executa o constructor do poema.js
-        poema.title = document.getElementById("title").value;
-        poema.image = document.getElementById("image").value;
-        poema.date = document.getElementById("date").value;
-        poema.text = document.getElementById("text").value;
-        // this.poemas.push(poema); //manda o poema que foi cadastrado para poemas
-        if (poema.add(poema)){
-            alert("Poema registrado!");
+        try {
+            var poema = new Poema(); // new Poema executa o constructor do poema.js
+            poema.title = document.getElementById("title").value;
+            poema.image = document.getElementById("image").value;
+            poema.date = document.getElementById("date").value;
+            poema.text = document.getElementById("text").value;
+            poema.add(poema);
+            alert("Poema Adicionado!");
+            this.getAllPoema();
+        } catch (ex) {
+            alert(ex);
         }
-        this.getAllPoema();
     },
-    getAllPoema: function () {
+
+    
+    getAllPoema:function () {
         var poema = new Poema;
         this.poemas = poema.getAll();
-        var tabela = "<table class='table'> <tr> <th>Título</th> <th>imagem</th> <th>Data</th> <th>Texto</th> </tr>";
+        var tabela = "<table class='table'> <tr> <th>Título</th> <th>Imagem</th> <th>Data</th> <th style= width:50%>Texto</th> </tr>";
 
         for (var index = 0; index < this.poemas.length; index++) {
-            tabela += "<tr> <td>" + this.poemas[index].nome + "</td> <td>" + this.poemas[index].descricao + "</td> <td>" + 
-             this.poemas[index].quant + "</td> <td> R$" + this.poema[index].valor.toFixed(2) + "</td> </tr>";
+            tabela += "<tr> <td>" + this.poemas[index].title + "</td> <td>" + this.poemas[index].image + "</td> <td>" + 
+            this.poemas[index].date + "</td> <td>" + this.poemas[index].text + "</td> </tr>";
         }
 
         tabela += "</table>";
         document.getElementById("listaPoemas").innerHTML = tabela
-    },
+        }
     }
 
   
 
 
     //listar poemas
+    
 
     //atualizr poemas
 
